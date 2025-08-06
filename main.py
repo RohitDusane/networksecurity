f6df4e6bba9ca460d90b463f2271be951ae54c8b
RohitDusane <stat.data247@gmail.com>
2025-08-06 13:44:10 +0530

    modified data validation main.py

--- main.py
+++ main.py
@@ -11,24 +11,37 @@ import sys
 
 if __name__ == '__main__':
     try:
-        training_pipeline_config = TrainingPipelineConfig()  # Create an instance
+        logging.info("Starting Network Security Training Pipeline")
+        # Set up the overall training pipeline config
+        training_pipeline_config = TrainingPipelineConfig()
+        
+        # Data Ingestion
         data_ingestion_config = DataIngestionConfig(training_pipeline_config)  # Pass the instance
         data_ingestion = DataIngestion(data_ingestion_config)
-        logging.info(f'Initiate Data Ingestion')
+
+        logging.info(f'Initiating data ingestion')
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
-        logging.info(f"Data Initaition completed")
+        logging.info("Data ingestion completed successfully")
+        logging.info(f"Data ingestion artifact details: {data_ingestion_artifact}")
         print(data_ingestion_artifact)
 
-        # data validation config
+        
+        # Data Validation
         data_validation_config =DataValidationConfig(training_pipeline_config)
         # Initialise Data Validation
         data_validation= DataValidation(data_ingestion_artifact, data_validation_config)
-        logging.info(f"Initiate data validation")
+        
+        logging.info(f"Initiating data validation")
         data_validation_artifact = data_validation.initiate_data_validation()
-        logging.info(f"Data Validation completed")
+        logging.info(f"Data validation completed successfully")
+        logging.info(f"Data validation artifact details: {data_validation_artifact}")
         print(data_validation_artifact)
 
+
+        logging.info("Pipeline execution(Data ingestion & validation) completed successfully")
+
     except Exception as e:
+        logging.exception("An error occurred during pipeline execution.")
         raise NetworkSecurityException(e,sys)
 
 
--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -6,15 +6,16 @@ from datetime import datetime
 # Setup logs directory and log file path
 log_dir = Path("logs")
 log_dir.mkdir(exist_ok=True)
-log_file = log_dir/f"{datetime.now().strftime("'%Y-%m-%d_%H-%M-%S")}.log"
+log_file = log_dir/f"{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log"
 
 # Configure handler
 # console_handler = logging.StreamHandler(sys.stdout)
 file_handler = logging.FileHandler(log_file, encoding="utf-8")
 hand = [file_handler]
 
-#Loggig format
-logging_str = "[%(asctime)s: %(lineno)d: %(name)s -%(levelname)s: -%(module)s: -%(message)s]"
+#Logging format
+logging_str = "[%(asctime)s] [%(lineno)d] [%(name)s] - %(levelname)s - %(module)s: %(message)s"
+
         
 # Basic Logging Configuration
 logging.basicConfig(
